1a10523041e1877ad2aa0607363b49d2401d3289
k-styles <kartikhanand@gmail.com>
2022-12-04 02:23:13 +0530

    add profile encoder

---
+++ src/data_processing/pre_processing/profile_encode.py
@@ -0,0 +1,9 @@
+import json
+
+profile_json = "json"   # get json here
+input_vec = []
+
+# Education degree
+match profile_json["education_degree"]
+if profile_json["education_degree"] == "btech":
+    input_vec
